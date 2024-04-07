3281b74054cb341e168afcbfca1c882aa97559d3
vovinski <info@it-gate.ru>
2024-04-07 11:40:27 +0000

    Add app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000;
+
+// Подключение к базе данных MySQL
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'username',
+  password: 'password',
+  database: 'mydatabase'
+});
+
+// Подключение к базе данных
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Connected to database');
+});
+
+// Маршрут для получения данных из базы данных
+app.get('/data', (req, res) => {
+  let sql = 'SELECT * FROM mytable';
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+// Запуск сервера
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
